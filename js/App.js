4c347184a2842d899aa831bbf556ab0794fd4a25
adr420 <aditya000000420@gmail.com>
2024-03-12 10:07:02 +0530

    Added more TODOs.

--- js/App.js
+++ js/App.js
@@ -4,11 +4,13 @@ const msgElem = document.querySelector("[data-message]");
 const sock = new WebSocket("ws://localhost:8080");
 
 // TODO:
+// 0. Restructure Server Code.
 // 1. Becomes buggy after 2-3 matches
 // 2. Dark mode
 // 3. Player Account
 // 4. Friends
 // 5. Chats
+// 6. Firebase integration	
 
 const App = {
 	init: function(){
